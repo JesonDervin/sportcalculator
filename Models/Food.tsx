be1f4600bcb53599a459abe95e21af29eadeef8b
JesonDervin <98985079+JesonDervin@users.noreply.github.com>
2022-04-10 21:54:53 +0200

    5 add an ingredient to a meal (#16)
    feat(daily): add a food to a meal

--- Models/Food.tsx
+++ Models/Food.tsx
@@ -1,9 +1,9 @@
-import NutritionalDetails from "./NutritionalDetails";
-
 export default class Food {
   constructor(
     public name: string = "",
     public quantity: number = 0,
-    public nutritionalValue: NutritionalDetails = new NutritionalDetails()
+    public protein: number = 0,
+    public carbohydrate: number = 0,
+    public lipid: number = 0
   ) {}
 }
