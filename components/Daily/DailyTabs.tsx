9461d1e3a1317e0c059d37120377e6f7fde17c11
JesonDervin <jeson.dervin@outlook.com>
2022-04-10 14:23:03 +0000

    adding basic form with name

--- components/Daily/DailyTabs.tsx
+++ components/Daily/DailyTabs.tsx
@@ -27,7 +27,7 @@ function TabPanel(props: TabPanelProps) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          <div>{children}</div>
         </Box>
       )}
     </div>
--- components/Food/AddIngredientDialog.tsx
+++ components/Food/AddIngredientDialog.tsx
@@ -7,7 +7,9 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  TextField,
 } from "@mui/material";
+import Food from "../../Models/Food";
 
 export default function FoodDialog() {
   const [open, setOpen] = React.useState(false);
@@ -17,6 +19,24 @@ export default function FoodDialog() {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const [currentIngredient, setCurrentIngredient] = React.useState<Food>(
+    new Food()
+  );
+  const saveIngredient = (e: React.FormEvent, formData: Food) => {
+    e.preventDefault();
+    console.log("saved", currentIngredient);
+    handleClose();
+  };
+
+  const handleNameChange = (
+    e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    setCurrentIngredient({
+      ...currentIngredient,
+      name: e.currentTarget.value,
+    });
   };
 
   return (
@@ -32,15 +52,23 @@ export default function FoodDialog() {
       </IconButton>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Add an Ingredient</DialogTitle>
-        <DialogContent>NOTE: FoodForm TO BE INSERTED HERE</DialogContent>
-        <DialogActions>
-          <Button color="error" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button color="primary" onClick={handleClose}>
-            Confirm
-          </Button>
-        </DialogActions>
+        <form onSubmit={(e) => saveIngredient(e, currentIngredient)}>
+          <DialogContent>
+            <TextField
+              label="name"
+              value={currentIngredient.name}
+              onChange={handleNameChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button color="error" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button color="primary" type="submit">
+              Confirm
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
