b1a72a0ae5f12059b2e869216796d6d3cee54de4
JesonDervin <98985079+JesonDervin@users.noreply.github.com>
2022-04-22 22:25:21 +0200

    feat(layout): adding menu tab and recipe route (#26)

---
+++ components/Layout/MainNavBar.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { useRouter } from "next/router";
+
+import { AppBar, Toolbar, CssBaseline, Tab, Tabs } from "@mui/material";
+import { useTranslation } from "next-i18next";
+import { EventAvailable, StickyNote2 } from "@mui/icons-material";
+const MainNavBar = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    router.push(newValue);
+  };
+  return (
+    <AppBar position="static" color="transparent">
+      <CssBaseline />
+      <Toolbar>
+        <div>
+          <Tabs
+            value={router.pathname}
+            onChange={handleChange}
+            aria-label="icon label tabs example"
+          >
+            <Tab icon={<EventAvailable />} label={t("menu.daily")} value="/" />
+            <Tab
+              icon={<StickyNote2 />}
+              label={t("menu.recipes")}
+              value="/Recipes"
+            />
+          </Tabs>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MainNavBar;
