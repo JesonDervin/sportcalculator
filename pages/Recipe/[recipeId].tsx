ef57b3429de8d22bfc97ace52454080ea55f5e74
JesonDervin <jeson.dervin@outlook.com>
2022-05-01 16:44:55 +0000

    feat(recipe): add recipe layout

--- pages/Recipe/[recipeId].tsx
+++ pages/Recipe/[recipeId].tsx
@@ -1,11 +1,43 @@
-import { useRouter } from "next/router";
+import * as React from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticPaths } from "next";
+import RecipeLayout from "../../components/Layout/RecipeLayout";
+import { useTranslation } from "next-i18next";
+import LocalStorageKeys from "../../src/Models/LocalStorageKeys";
+import Recipe from "../../src/Models/Recipe";
+import { useLocalStorage } from "usehooks-ts";
+import { useRouter } from "next/router";
 
 const EditRecipe = () => {
+  const { t } = useTranslation();
   const router = useRouter();
-  console.log("router query", router.query);
-  return <div>help</div>;
+  const [storedRecipes] = useLocalStorage<Recipe[]>(
+    LocalStorageKeys.Recipes,
+    []
+  );
+
+  const { recipeId } = router.query;
+  // * since storedRecipes are from window object,
+  // * we have to use this trick to not make a mismatch
+  //* between what is generated server side and what is on client,
+  //*  to not make virtual dom confused
+  const currentRecipeRef = React.useRef<Recipe>();
+  React.useEffect(() => {
+    currentRecipeRef.current = storedRecipes.find(
+      (recipe: Recipe) => recipe.id === recipeId
+    );
+  });
+
+  return (
+    <RecipeLayout>
+      <h1>{t("recipe.edit")}</h1>
+      {currentRecipeRef.current ? (
+        <div>Edit Recipe Form</div>
+      ) : (
+        <div>{t("errors.invalid")}</div>
+      )}
+    </RecipeLayout>
+  );
 };
 
 export default EditRecipe;
