ec7eac40e8cc2abc6697df36c9df88328749ae5a
JesonDervin <jeson.dervin@outlook.com>
2022-05-22 14:47:03 +0000

    adding getlocal storage value

--- components/Daily/Daily.tsx
+++ components/Daily/Daily.tsx
@@ -6,15 +6,26 @@ import { MealType } from "../../src/Models/MealType";
 import Meal from "../../src/Models/Meal";
 import { useTranslation } from "next-i18next";
 import DailyMeals from "../../src/Models/DailyMeals";
+import { useLocalStorage } from "usehooks-ts";
+import LocalStorageKeys from "../../src/Models/LocalStorageKeys";
+import { DateTime } from "luxon";
 
 export default function Daily() {
   const { t } = useTranslation();
-  const dailyMeals = new DailyMeals(
-    new Meal(MealType.Breakfast),
-    new Meal(MealType.Lunch),
-    new Meal(MealType.Snack),
-    new Meal(MealType.Dinner)
+  const [storedDailies, setStoredDailies] = useLocalStorage<DailyMeals[]>(
+    LocalStorageKeys.Dailies,
+    []
   );
+  const today = DateTime.now().toISODate();
+  const todayStoredDaily = storedDailies.filter((d) => d.date === today)[0];
+  const dailyMeals =
+    todayStoredDaily ??
+    new DailyMeals(
+      new Meal(MealType.Breakfast),
+      new Meal(MealType.Lunch),
+      new Meal(MealType.Snack),
+      new Meal(MealType.Dinner)
+    );
   return (
     <Container>
       <Box>{t("daily.title")}</Box>
