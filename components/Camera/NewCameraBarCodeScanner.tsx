756dbb3a18d603b56165cb0b318b1ea6e4645961
JesonDervin <jeson.dervin@outlook.com>
2022-06-26 10:13:57 +0000

    adding better scan

---
+++ components/Camera/NewCameraBarCodeScanner.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Html5Qrcode } from "html5-qrcode"
+import { Html5QrcodeError, QrcodeErrorCallback, QrcodeSuccessCallback } from "html5-qrcode/esm/core";
+import { Html5QrcodeCameraScanConfig } from "html5-qrcode/esm/html5-qrcode";
+interface CameraBarCodeScannerProps {
+    OnBarcodeDetected: (barcode: string) => void;
+}
+
+const NewCameraBarCodeScanner = (props: CameraBarCodeScannerProps) => {
+    const { OnBarcodeDetected } = props;
+
+
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } } as Html5QrcodeCameraScanConfig;
+    React.useEffect(() => {
+        const html5QrCode = new Html5Qrcode("reader");
+        Html5Qrcode.getCameras().then(devices => {
+            if (devices && devices.length) {
+                const barCodeSuccessCallback: QrcodeSuccessCallback = (decodedText, decodedResult) => {
+                    // todo exploit decodedResult to filter format
+                    OnBarcodeDetected(decodedText);
+                };
+
+                const barCodeErrorCallback: QrcodeErrorCallback = (errorMessage: string, error: Html5QrcodeError) => {
+                    // todo: show error
+                }
+                // prefer back camera
+                html5QrCode.start({ facingMode: "environment" }, config, barCodeSuccessCallback, barCodeErrorCallback);
+            }
+        });
+
+        return () => { html5QrCode.stop().then(() => { console.log("stopped") }) };
+    });
+
+    return (<div id="reader"></div>)
+};
+
+export default NewCameraBarCodeScanner;
--- src/Services/OpenFoodFactService.ts
+++ src/Services/OpenFoodFactService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Food from "../Models/Food";
-import { OpenFoodFactApiResponse } from "../Models/OpenFoodFactApiResponse";
+import { OpenFoodFactApiProduct, OpenFoodFactApiResponse } from "../Models/OpenFoodFactApiResponse";
 
 export default class OpenFoodFactService {
   private OpenFoodFactBaseUrl = "https://[locale].openfoodfacts.org";
@@ -22,19 +22,55 @@ export default class OpenFoodFactService {
       throw "unknown food";
     }
     const responseData = data as OpenFoodFactApiResponse;
-    const convertedFood = this.mapFoodApiToFood(responseData);
+    console.log("resp", responseData);
+    const convertedFood = this.mapFoodApiToFood(responseData, locale, barCode);
     return convertedFood;
   }
 
   // Todo: maybe move this part to a mapper
-  public mapFoodApiToFood(apiFood: OpenFoodFactApiResponse): Food {
+  public mapFoodApiToFood(apiFood: OpenFoodFactApiResponse, locale: string, barCode: string): Food {
     const product = apiFood.product;
     const nutriments = product.nutriments;
+    const foodName = this.getFoodName(apiFood, locale, barCode);
     return new Food(
-      product.product_name,
+      foodName,
       nutriments.proteins_100g,
       nutriments.carbohydrates_100g,
       nutriments.fat_100g
     );
+  }
+
+  // *  get name from api, if not found, return barcode
+  public getFoodName(apiFood: OpenFoodFactApiResponse, locale: string, barCode: string): string {
+    console.log("getFoodName", apiFood)
+    let name = "";
+    const genericTranslatedName = `generic_name_${locale}` as keyof OpenFoodFactApiProduct;
+    console.group()
+    console.log("genericTranslatedName", genericTranslatedName);
+    name = apiFood.product[genericTranslatedName] as string;
+    console.log("name", name);
+    console.groupEnd()
+    if (this.nameIsNullOrWhiteSpace(name)) {
+      name = apiFood.product.generic_name;
+      if (this.nameIsNullOrWhiteSpace(name)) {
+        console.group()
+        const productTranslatedName = `product_name_${locale}` as keyof OpenFoodFactApiProduct;
+        console.log("productTranslatedName", productTranslatedName);
+        name = apiFood.product[productTranslatedName] as string;
+        console.log("name", name);
+        console.groupEnd()
+        if (this.nameIsNullOrWhiteSpace(name)) {
+          name = apiFood.product.product_name as string;
+          if (this.nameIsNullOrWhiteSpace(name)) {
+            name = barCode;
+          }
+        }
+      }
+    }
+    return name;
+  }
+
+  public nameIsNullOrWhiteSpace(name: string): boolean {
+    return name == null || name.trim() === '';
   }
 }
