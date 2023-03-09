d91e835d07b0b88182fc1f901bfcae6776767347
JesonDervin <jeson.dervin@outlook.com>
2023-03-09 07:43:00 +0000

    feat: #90 setup database

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,12 +1,35 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use pickledb::PickleDb;
+use std::path::Path;
+
+static DB_PATH: &str = "db/sports.db";
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    // todo having a repository that manage that
+    let db = PickleDb::load(
+        DB_PATH,
+        pickledb::PickleDbDumpPolicy::AutoDump,
+        pickledb::SerializationMethod::Json,
+    )
+    .unwrap();
+
+    let response = db.get::<String>("echo").unwrap();
+    HttpResponse::Ok().body(response)
 }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
+    // todo having a repository that manage that
+    let mut db = PickleDb::load(
+        DB_PATH,
+        pickledb::PickleDbDumpPolicy::AutoDump,
+        pickledb::SerializationMethod::Json,
+    )
+    .unwrap();
+
+    db.set("echo", &req_body.clone()).unwrap();
+
     HttpResponse::Ok().body(req_body)
 }
 
@@ -16,6 +39,18 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Setting up DB
+    let file_exists: bool = Path::new(DB_PATH).exists();
+
+    if !file_exists {
+        PickleDb::new(
+            DB_PATH,
+            pickledb::PickleDbDumpPolicy::AutoDump,
+            pickledb::SerializationMethod::Json,
+        );
+    }
+
+    // Http server starting
     HttpServer::new(|| {
         App::new()
             .service(hello)
